2b06ec6aeb9e4536cacf4ac4e737fb3fa2f9ef46
Alp Tarla <alptarla@gmail.com>
2021-08-01 14:11:07 +0300

    Implement redux toolkit for state managment

--- src/components/post/PostList.jsx
+++ src/components/post/PostList.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { fetchPosts } from '../../services/api'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPosts, selectPost } from '../../store/slices/post'
 import PostItem from './PostItem'
 import classes from './PostList.module.css'
 
 function PostList() {
-  const [posts, setPosts] = useState([])
-  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+  const { posts, loading } = useSelector(selectPost)
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])
 
   const handleToggleBookmark = () => {
     // toggle bookmark
@@ -14,13 +19,6 @@ function PostList() {
   const handleToggleLike = () => {
     // toggle like
   }
-
-  useEffect(() => {
-    setLoading(true)
-    fetchPosts()
-      .then((res) => setPosts(res))
-      .finally(() => setLoading(false))
-  }, [])
 
   if (loading) return <div>...loading</div>
 
---
+++ src/store/slices/post.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import * as apiService from '../../services/api'
+
+export const getPosts = createAsyncThunk('post/fetchPosts', () => {
+  return apiService.fetchPosts()
+})
+
+const postSlice = createSlice({
+  name: 'post',
+  initialState: {
+    posts: [],
+    loading: false,
+    error: null,
+  },
+  extraReducers: {
+    [getPosts.fulfilled](state, action) {
+      state.posts = action.payload
+      state.loading = false
+    },
+    [getPosts.pending](state) {
+      state.loading = true
+    },
+  },
+})
+
+export const selectPost = (state) => state.post
+
+export default postSlice.reducer
