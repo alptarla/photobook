81d2f5a8ec04c84aa16c5566aaf616a0c1b43acf
Alp Tarla <alptarla@gmail.com>
2021-08-01 12:12:35 +0300

    Remove command lines

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,7 @@ import Container from '../components/ui/Container'
 function Home() {
   return (
     <Container>
-      {/* search, filter, toggle layout */}
       <PostList />
-      {/* pagination */}
     </Container>
   )
 }
